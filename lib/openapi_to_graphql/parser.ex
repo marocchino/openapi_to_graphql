6c783eede23dfc46eaa93e67b43070ed4f32a602
marocchino <marocchino@users.noreply.github.com>
2021-06-02 10:29:53 +0900

    🐛 Use nullable instead of required

--- lib/openapi_to_graphql/parser.ex
+++ lib/openapi_to_graphql/parser.ex
@@ -7,10 +7,8 @@ defmodule Parser do
         """
         # imported from: #{path}
         #{content["type"]} #{object_type(content)} do
-        #{
-          Enum.map(content["properties"], &field(&1, content["required"] || [], path))
-          |> Enum.join("\n")
-        }
+        #{Enum.map(content["properties"], &field(&1, path))
+        |> Enum.join("\n")}
         end
         """
 
@@ -28,31 +26,40 @@ defmodule Parser do
 
   def object_type(content), do: ":#{content["title"] |> Macro.underscore()}"
 
-  def field({name, spec}, required, path) do
-    "  field(:#{name}, #{type(spec, name not in required, path)}#{
-      description(spec["description"])
-    })"
+  def field({name, spec}, path) do
+    "  field :#{name}, #{type(spec, path)}#{description(spec["description"])}"
   end
 
-  def type(%{"type" => "array", "items" => %{"$ref" => ref}}, _, path) do
+  def type(%{"type" => "array", "items" => %{"$ref" => ref}, "nullable" => true}, path) do
+    {:ok, content} = YamlElixir.read_from_file("#{Path.dirname(path)}/#{ref}")
+
+    "list_of(non_null(#{object_type(content)}))"
+  end
+
+  def type(%{"type" => "array", "items" => %{"$ref" => ref}}, path) do
     {:ok, content} = YamlElixir.read_from_file("#{Path.dirname(path)}/#{ref}")
 
     "non_null(list_of(non_null(#{object_type(content)})))"
   end
 
-  def type(%{"type" => "array", "items" => items}, _, path) do
+  def type(%{"type" => "array", "items" => _items}, _path) do
     "non_null(list_of(non_null(:inline_object)))"
   end
 
-  def type(spec, false, path), do: "non_null(#{type(spec, true, path)})"
-
-  def type(%{"$ref" => ref}, true, path) do
+  def type(%{"$ref" => ref, "nullable" => true}, path) do
     {:ok, content} = YamlElixir.read_from_file("#{Path.dirname(path)}/#{ref}")
 
     "#{object_type(content)}"
   end
 
-  def type(spec, true, _), do: ":#{spec["format"] || spec["type"]}"
+  def type(%{"$ref" => ref}, path) do
+    {:ok, content} = YamlElixir.read_from_file("#{Path.dirname(path)}/#{ref}")
+
+    "non_null(#{object_type(content)})"
+  end
+
+  def type(%{"nullable" => true} = spec, path), do: ":#{spec["format"] || spec["type"]}"
+  def type(spec, _), do: "non_null(:#{spec["format"] || spec["type"]})"
 
   def description(nil), do: ""
   def description(str), do: ", description: #{inspect(str)}"
